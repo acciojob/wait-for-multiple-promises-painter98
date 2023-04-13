bf2a6818b62cb967ea4bc52337c29e4484889beb
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-13 21:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 
 let promise1 = new Promise((resolve,reject)=>{
 	let time = Math.floor(Math.random()*3);
@@ -32,44 +31,8 @@ let promise3 = new Promise((resolve,reject)=>{
 })
 
 Promise.all([promise1,promise2,promise3]).then((values)=>{
-   Document.getElementById('first').innerText=values[0].promise1;
-}).catch((error)=>{
-    console.log(error);
-});
-console.log(result);
-let promise1 = new Promise((resolve,reject)=>{
-	let time = Math.floor(Math.random()*3);
-    setTimeout(()=>{
-        let obj = {
-            promise1:'promise 1',
-            time:time
-        }
-        resolve(obj);
-    },time*1000);
-})
-let promise2 = new Promise((resolve,reject)=>{
-	let time = Math.floor(Math.random()*3);
-    setTimeout(()=>{
-        let obj = {
-            promise2:'promise 2',
-            time:time
-        }
-        resolve(obj);
-    },time*1000);
-})
-let promise3 = new Promise((resolve,reject)=>{
-	let time = Math.floor(Math.random()*3);
-    setTimeout(()=>{
-        let obj = {
-            promise3:'promise 3',
-            time:time
-        }
-        resolve(obj);
-    },time*1000);
-})
-
-Promise.all([promise1,promise2,promise3]).then((values)=>{
-   Document.getElementById('first').innerText=values[0].promise1;
+   Document.getElementById('second').innerText=values[0];
+	
 }).catch((error)=>{
     console.log(error);
 });
