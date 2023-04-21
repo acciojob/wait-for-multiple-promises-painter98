a6dc85e2c8cba759163aec3aafbd1a43a717fe64
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-21 23:05:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let promise1 = new Promise((resolve,reject)=>{
-	let time = Math.floor(Math.random()*3);
+	let time = Math.random()*3;
     setTimeout(()=>{
         let obj = {
             promise1:'promise 1',
@@ -10,7 +10,7 @@ let promise1 = new Promise((resolve,reject)=>{
     },time*1000);
 })
 let promise2 = new Promise((resolve,reject)=>{
-	let time = Math.floor(Math.random()*3);
+	let time = Math.random()*3;
     setTimeout(()=>{
         let obj = {
             promise2:'promise 2',
@@ -20,7 +20,7 @@ let promise2 = new Promise((resolve,reject)=>{
     },time*1000);
 })
 let promise3 = new Promise((resolve,reject)=>{
-	let time = Math.floor(Math.random()*3);
+	let time = Math.round(Math.random()*3,3);
     setTimeout(()=>{
         let obj = {
             promise3:'promise 3',
@@ -31,10 +31,10 @@ let promise3 = new Promise((resolve,reject)=>{
 })
 
 Promise.all([promise1,promise2,promise3]).then(([a,b,c])=>{
-    document.getElementById('loading1').innerText = a.time;
-	document.getElementById('loading2').innerText = b.time;
-	document.getElementById('loading3').innerText = c.time;
-   document.getElementById('result').innerText = a.time+b.time+c.time;
+    document.getElementById('Promise 1Loading').innerText = a.time;
+	document.getElementById('Promise 2Loading').innerText = b.time;
+	document.getElementById('Promise 3Loading').innerText = c.time;
+   document.getElementById('Loading').innerText = a.time+b.time+c.time;
 	
 }).catch((error)=>{
     console.log(error);
